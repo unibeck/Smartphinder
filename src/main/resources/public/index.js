f4fab18c5bdb01f3e8b44a63629d63f5711e6db4
Unknown <jonathantbeckman@gmail.com>
2016-11-27 22:45:08 -0500

    Finished form UI and post request works

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,4 +1,4 @@
-angular.module('SmartPhinder', ['ngMaterial']);
+angular.module('SmartPhinder', ['ngMaterial', 'ngAnimate']);
 
 angular.module('SmartPhinder').config(function ($mdThemingProvider) {
 	$mdThemingProvider.theme('default')
@@ -11,7 +11,82 @@ angular.module('SmartPhinder').config(function ($mdThemingProvider) {
 		.warnPalette('red');
 });
 
-angular.module('SmartPhinder').controller('AppCtrl', function ($scope) {
+angular.module('SmartPhinder').controller('AppCtrl', function ($scope, $http) {
+	var numOfConstraints = 9;
+	$scope.currentQuestion = 0;
 
+	$scope.nextQuestion = function() {
+		$scope.numOfValidConstraints = checkUserConstraint();
+		$scope.currentQuestion++;
+	}
 
+	$scope.previousQuestion = function() {
+		$scope.numOfValidConstraints = checkUserConstraint();
+		$scope.currentQuestion--;
+	}
+
+	$scope.userConstraint = {
+		"brand": "",
+		"os": "",
+		"price": 0,
+		"battery": 0,
+		"camera": 0,
+		"ram": 0,
+		"storage": 0,
+		"resolution": 0,
+		"display-size": 0
+	}
+
+	$scope.numOfValidConstraints = 0;
+
+	function checkUserConstraint() {
+		var count = 0;
+
+		if ($scope.userConstraint.brand) {
+			count++;
+		}
+		if($scope.userConstraint.os) {
+			count++;
+		}
+		if($scope.userConstraint.price) {
+			count++;
+		}
+		if($scope.userConstraint.battery) {
+			count++;
+		}
+		if($scope.userConstraint.camera) {
+			count++;
+		}
+		if($scope.userConstraint.ram) {
+			count++;
+		}
+		if($scope.userConstraint.storage) {
+			count++;
+		}
+		if($scope.userConstraint.resolution) {
+			count++;
+		}
+		if($scope.userConstraint['display-size']) {
+			count++;
+		}
+
+		return (count * 11) + 1;
+	}
+
+	$scope.response = undefined;
+
+	var config = {
+    headers : {
+      'Content-Type': 'application/json'
+    }
+  }
+
+	$scope.submitConstraints = function() {
+		$http.post("http://localhost:8080/smartphone/constraint", $scope.userConstraint, config)
+			.then(
+				function (result) {
+					console.log(result);
+					$scope.response = result.data;
+				});
+	}
 });
